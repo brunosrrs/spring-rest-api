e8033b7fa8eb9622c17c0603e4ace1a7817e5d8b
brunosrrs <liucristinas74@gmail.com>
2022-02-01 11:27:03 -0300

    Implementando API do Pedido

--- src/main/java/io/github/brunosrrs/domain/entity/Cliente.java
+++ src/main/java/io/github/brunosrrs/domain/entity/Cliente.java
@@ -2,11 +2,16 @@ package io.github.brunosrrs.domain.entity;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Set;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 //anotation para dizer que é uma entidade JPA
 @Entity
 //@Table usada a menos que o nome da entidade seja diferente do nome da tabela;
@@ -31,56 +36,9 @@ public class Cliente {
     @OneToMany(mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
 
-    public Cliente() {
-    }
-
-    public Set<Pedido> getPedidos() {
-        return pedidos;
-    }
-
-    public void setPedidos(Set<Pedido> pedidos) {
-
-        this.pedidos = pedidos;
-    }
-
     public Cliente(Integer id, String nome) {
         this.id = id;
         this.nome = nome;
     }
 
-    public Cliente(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                '}';
-    }
 }
---
+++ src/main/java/io/github/brunosrrs/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,16 @@
+package io.github.brunosrrs.rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemPedidoDTO {
+
+    private Integer produto;
+    private Integer quantidade;
+}
