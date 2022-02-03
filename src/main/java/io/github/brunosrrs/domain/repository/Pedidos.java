a7cb724089e1fd08fc2e734a6dd35f6c069541b0
brunosrrs <liucristinas74@gmail.com>
2022-02-03 11:31:44 -0300

    Implementando descricao do Pedido

--- src/main/java/io/github/brunosrrs/domain/repository/Pedidos.java
+++ src/main/java/io/github/brunosrrs/domain/repository/Pedidos.java
@@ -2,6 +2,14 @@ package io.github.brunosrrs.domain.repository;
 
 import io.github.brunosrrs.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface Pedidos extends JpaRepository <Pedido, Integer> {
+
+    //pode ser que o pedido nao exista por isso OPTIONAL
+    @Query("select p from Pedido p left join fetch p.itens where p.id = :id")
+    Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
 }
--- src/main/java/io/github/brunosrrs/service/PedidoService.java
+++ src/main/java/io/github/brunosrrs/service/PedidoService.java
@@ -3,7 +3,11 @@ package io.github.brunosrrs.service;
 import io.github.brunosrrs.domain.entity.Pedido;
 import io.github.brunosrrs.rest.dto.PedidoDTO;
 
+import java.util.Optional;
+
 public interface PedidoService {
 
     Pedido salvar (PedidoDTO dto);
+
+    Optional<Pedido> obterPedidoCompleto (Integer id);
 }
